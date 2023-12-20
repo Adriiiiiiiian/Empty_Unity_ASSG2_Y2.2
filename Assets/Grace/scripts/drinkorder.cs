e853b5d0aea0257f5cde4d32122cc9ae206f9e57
Gracefoo06 <s10243108@connect.np.edu.sg>
2023-12-20 10:46:22 +0800

    code on ordering drinks

--- Assets/Grace/scripts/drinkorder.cs
+++ Assets/Grace/scripts/drinkorder.cs
@@ -1,10 +1,40 @@
+/*
+ * Author: Grace Foo
+ * Date: 18/12/2023
+ * Description: this handles ordering the drinks
+ */
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class drinkorder : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_InputField orderInput;
+    public GameObject pengDrink;
+    public GameObject orderWrong;
+    public GameObject hotdrink;
+    public void CheckOrder()
+    {
+        string userInput = orderInput.text.ToLower();
+        if (userInput.Contains("teh") || userInput.Contains("kopi"))
+        {
+            if (userInput.Contains("peng"))
+            {
+                pengDrink.SetActive(true);
+            }
+            else
+            {
+                hotdrink.SetActive(true);
+            }
+        }
+        else
+        {
+          orderWrong.SetActive(true);
+        }
+    }
+        // Start is called before the first frame update
     void Start()
     {
         
